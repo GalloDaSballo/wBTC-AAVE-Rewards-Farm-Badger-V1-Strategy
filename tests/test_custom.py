416fd768ba811fbbfeb7ee947d0db02547232885
Entreprenerd <valeranijr@gmail.com>
2021-06-14 18:19:36 +0200

    feat: with custom test

--- tests/test_custom.py
+++ tests/test_custom.py
@@ -1,8 +1,7 @@
 import brownie
 from brownie import *
 from helpers.constants import MaxUint256
-from helpers.SnapshotManager import SnapshotManager
-from helpers.time import days
+import pytest
 
 """
   TODO: Put your tests here to prove the strat is good!
@@ -10,6 +9,25 @@ from helpers.time import days
   See test_strategy_permissions, for tests at the permissions level
 """
 
+@pytest.fixture
+def aavePool():
+  return Contract.from_explorer("0x7d2768dE32b0b80b7a3454c06BdAc94A69DDc7A9")
 
-def test_my_custom_test(deployed):
-  assert False
+@pytest.fixture
+def aaveRewards():
+  return Contract.from_explorer("0xd784927Ff2f95ba542BfC824c8a8a98F3495f6b5")
+
+
+def test_my_custom_test(want, deployer, vault, strategy, aavePool, aaveRewards):
+  balance = want.balanceOf(deployer)
+  want.approve(vault, balance, {"from": deployer})
+  vault.deposit(balance, {"from": deployer})
+  vault.earn({"from": deployer})
+
+  aToken = Contract.from_explorer(strategy.aToken())
+
+  assert strategy.balanceOfPool() == aToken.balanceOf(strategy)
+
+  ## If we deposited, then we must have some rewards
+
+  assert aaveRewards.getRewardsBalance([aToken], strategy) > 0
